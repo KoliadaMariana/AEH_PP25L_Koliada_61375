090d158f6d94b7147dcddb521bf5ffcd7dce7264
KoliadaMariana <koliami7499_aehit@studnts.vizja.pl>
2025-03-26 15:00:57 +0100

    Pierwszy zapis do repozytorium Lab2

---
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -0,0 +1,12 @@
+package pl.pp;
+public class mojaDrugaAplikacja {
+    public static void main(String[] args){
+        int x = 10;
+        int result1 = x * 2;
+        int result2 = x * x;
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność x = " + result1);
+        System.out.println("x^2 = " + result2);
+
+    }
+}
