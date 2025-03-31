66671759cb51a9ee9bf3766c7a9f7aa9a835d649
mariana.koliada <koliami7499_aehit@students.vizja.pl>
2025-03-31 20:48:41 +0200

    Pierwszy zapis do repozytorium Lab4

---
+++ src/pl/pp/mojaCzwartaAplikacja.java
@@ -0,0 +1,78 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mojaCzwartaAplikacja {
+    /*public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+        System.out.print("Enter lower and upper integer limit: ");
+        int dolny = sc.nextInt();
+        int gorny = sc.nextInt();
+        int x = dolny, sum1 = dolny * dolny;
+        while (dolny != gorny) {
+            dolny += 1;
+            sum1 += (dolny * dolny);
+        }
+        System.out.println("The sums of the squares from");
+        System.out.println(x * x + " to " + gorny * gorny + " is " + sum1);
+        break;
+    }
+    while(true){
+System.out.println("Enter next set of limits: ");
+    int dolny1 = sc.nextInt();
+    int gorny1 = sc.nextInt();
+    int sum2 = dolny1 * dolny1, y = dolny1;
+    if (dolny1 == gorny1) {
+        System.out.println("Done");
+        break;
+    }
+    else {
+        while (dolny1 != gorny1) {
+        dolny1 += 1;
+        sum2 += (dolny1 * dolny1);
+        }
+        System.out.println("The sums of the squares from");
+        System.out.println( y * y + " to " + gorny1 * gorny1 + " is " + sum2);
+
+    }
+    sc.close();
+    }*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Prosze wybracz operacje ( lub -1, aby zalonczyć " + "\n" );
+        while ( true) {
+            System.out.println("dodawanie - 1" + "\n");
+            System.out.println("odejmowanie - 2" + "\n");
+            System.out.println("mnożemie - 3" + "\n");
+            System.out.println("dzielenie - 4" + "\n");
+            int a = sc.nextInt();
+            if (a != 1 && a != 2 && a != 3 && a != 4) {
+                break;
+            }
+            if ( a == -1) {
+                break;
+            }
+            System.out.println(" Prosze podać dzwie liczby (lub -1, aby zakonczyć)");
+            int l1 = sc.nextInt();
+            int l2 = sc.nextInt();
+            if (a == 1) {
+                System.out.println(l1 + l2);}
+                else if (a == 2) {
+                    System.out.println((l1 - l2) + " lub " + (l2 - l1));
+                } else if (a == 3) {
+                    System.out.println((l1 * l2));
+                } else if  (a == 4) {
+                    System.out.println((l1 / l2) + " lub " + (l2 / l1));
+                }
+                System.out.println("Prosze podać dwie liczby" + "\n");
+            }
+        }
+
+    }
+
+
+
+
+
